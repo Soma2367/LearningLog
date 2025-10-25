8853a3cf7cc9a195269fe87cbf55762c82d3c2af
soma matsumoto <souma67jiji@icloud.com>
2025-10-26 02:28:45 +0900

    feat: 学生・教師用ルート更新

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\DailyStudyLogController;
+use App\Http\Controllers\Student\DailyStudyLogController;
+use App\Http\Controllers\Teacher\TeacherController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,13 +14,14 @@ Route::get('/dashboard', function () {
     if (Auth::check() && Auth::user()->isTeacher()) {
         return redirect()->route('teacher.dashboard');
     }
-    return redirect()->route('student.dashboard');
+    return redirect()->route('student.daily_study_logs.index');
 })->middleware('auth')->name('dashboard');
 
 Route::middleware(['auth', 'teacher'])->prefix('teacher')->name('teacher.')->group(function() {
-    Route::get('/dashboard', function() {
-        return view('teacher.dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard', [TeacherController::class, 'dashboard'])->name('dashboard');
+     Route::get('/student_logs/{student}', [TeacherController::class, 'studentLogs'])->name('student.logs');
+     Route::get('/feedback/{log}', [TeacherController::class, 'showFeedback'])->name('feedback.show');
+     Route::post('/feedback/{log}', [TeacherController::class, 'storeFeedback'])->name('feedback.store');
 });
 
 Route::middleware(['auth', 'student'])->prefix('student')->name('student.')->group(function() {
@@ -28,7 +30,6 @@ Route::middleware(['auth', 'student'])->prefix('student')->name('student.')->gro
     })->name('dashboard');
     Route::resource('daily_study_logs', DailyStudyLogController::class);
 });
-
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
