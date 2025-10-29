fa99d4bed8e3a2f64176c5aee859a4c6e132db54
soma matsumoto <souma67jiji@icloud.com>
2025-10-29 17:10:09 +0900

    style: ボタンスタイル変更

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,7 +1,7 @@
 @props(['type' => 'button', 'variant' => 'primary', 'href' => null]);
 
 @php
- $baseClasses = 'inline-flex items-center px-5 py-3 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150';
+ $baseClasses = 'inline-flex items-center px-5 py-3  border border-transparent rounded-lg font-semibold text-base text-white uppercase tracking-widest focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150';
 @endphp
 
 @php
